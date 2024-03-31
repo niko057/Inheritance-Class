e313e7f75a694ad9ac6b8ff2f951246998104011
niko057 <nicatnamazov57.nn@gmail.com>
2024-03-31 23:35:19 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Inheritance_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Student st = new Student();
+
+            
+            st.Grades = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            Console.WriteLine(string.Join(", ", st.Grades));
+        }
+    }
+    public class Student
+    {
+        private int[] _grades;
+
+        public int[] Grades
+        {
+            get { return _grades; }
+            set
+            {
+                
+                _grades = value.Where(x => x % 2 == 0).ToArray();
+            }
+        }
+    }
+}
